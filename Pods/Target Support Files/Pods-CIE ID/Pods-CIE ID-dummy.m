1a246339513be4f886b8c7f07ec252c7339e0322
Pierluigi De Gregorio <piero@MBPdiPierluigi.intranet.bit4id.com>
2020-05-25 13:08:02 +0200

    risolto issue #76

---
+++ Pods/Target Support Files/Pods-CIE ID/Pods-CIE ID-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CIE_ID : NSObject
+@end
+@implementation PodsDummy_Pods_CIE_ID
+@end
---
+++ Pods/Target Support Files/Pods-cie-pkcs11/Pods-cie-pkcs11-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cie_pkcs11 : NSObject
+@end
+@implementation PodsDummy_Pods_cie_pkcs11
+@end
